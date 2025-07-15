3c053b4a0e8ddf6eddd275e3f4583109f0490d2f
Om Thote <omthote24@gmail.com>
2025-07-16 00:33:36 +0530

    Update API URLs for production

--- src/db.ts
+++ src/db.ts
@@ -46,29 +46,3 @@ export const TagModel = mongoose.model("Tag", TagSchema);
 
 
 
-/*import mongoose, {model, Schema} from "mongoose";
-
-mongoose.connect("mongodb+srv://omthote24:om123@cluster0.4rk615r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
-const UserSchema = new Schema({
-    username: {type: String, unique: true},
-    password: String
-})
-
-export const UserModel = model("User", UserSchema);
-
-const ContentSchema = new Schema({
-    title: String,
-    link: String,
-    tags: [{type: mongoose.Types.ObjectId, ref: 'Tag'}],
-    type: String,
-    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true },
-})
-
-const LinkSchema = new Schema({
-    hash: String,
-    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true, unique: true },
-})
-
-export const LinkModel = model("Links", LinkSchema);
-export const ContentModel = model("Content", ContentSchema);*/
